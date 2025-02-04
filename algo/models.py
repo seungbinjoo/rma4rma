5eee30be58c27e6f302a3511d3e109857f31e7bf
seungbinjoo <binnitude@gmail.com>
2025-02-04 18:05:04 +0000

    model architecture fixes

--- algo/models.py
+++ algo/models.py
@@ -5,6 +5,7 @@ from torch.distributions.normal import Normal
 from torch.utils.tensorboard import SummaryWriter
 from mani_skill.utils.common import flatten_state_dict
 import wandb
+from einops import rearrange
 
 # Neural network initialization method
 def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
@@ -38,13 +39,14 @@ class Agent(nn.Module):
             )
         # Adaptation training or evaluation phase
         if self.phase == "AdaptationTraining" or "Evaluation":
-            self.adaptation_net = AdaptationNet(proprio_in_dim=50, adapt_out_dim=16, use_depth=True)
+            self.adaptation_net = AdaptationNet(proprio_hist_dim=50, adapt_out_dim=output_dim)
             features_dim = (
                 observation_space_dim
-                - 37 - 32 # minus sensor_param dim and sensor_data dim
-                - (self.env_params_dim + 2) # privileged info
+                - 32*32 - 37 # minus sensor_data dim and sensor_param dim
+                - 50*26 - 1 # minus proprio and proprio_idx
                 + 2*self.obj_emb_dim + self.env_params_dim # object instance and category embeddings + env params
                 - 4 # after env encoder
+                - 11 # FIXME: make these calculations here clearer and more readable
             )
         else:
             raise ValueError("Phase must be policy training, adaptation training, or evaluation.")
@@ -109,7 +111,8 @@ class Agent(nn.Module):
     def action_to_proprio(self, obs):
         # insert previous action into proprioceptive (observation-action) history
         proprio_idx = obs['proprio_idx']
-        obs['proprio'][:, proprio_idx, self.action_space_dim:] = self.action
+        env_idx = torch.arange(obs['proprio'].shape[0]) # [0, 1, 2, ..., 511]
+        obs['proprio'][env_idx, proprio_idx, -self.action_space_dim:] = self.action
         return obs
 
     def get_value(self, obs):
@@ -167,34 +170,29 @@ def compute_env_encoder_dims(env_id, obj_emb_dim):
     return input_dim, output_dim
 
 class AdaptationNet(nn.Module):
-    def __init__(self,
-                 observation_space=None, # TODO: use this?
-                 proprio_in_dim=50, # 50 timesteps for proprioceptive data (note: proprioceptive feature dimension is 39)
-                 adapt_out_dim=16,
-                 use_depth=False):
-
+    def __init__(
+            self,
+            proprio_hist_dim=50, # 50 timesteps in each proprioceptive observation
+            proprio_features_dim = 26, # feature dim is 26 for each proprioceptive observation
+            adapt_out_dim=69, # by default 69, which is the env embedding dim for "PickSingleYCBRMA-v1" task
+        ):
         super(AdaptationNet, self).__init__()
-        self.use_depth = use_depth
-        depth_cnn_out_dim = 0
-        camera_param_dim = 0
-        if use_depth:
-            depth_cnn_out_dim = 64
-            camera_param_dim = 32 + 9  # 16 + 16 + 9
+        depth_cnn_out_dim = 64
+        camera_param_dim = 3*4 + 4*4 + 3*3 # extrinsic_cv, cam2world_gl, instrinsic_cv, dim = 37
         self.depth_cnn = DepthCNN(out_dim=depth_cnn_out_dim)
-        self.proprio_cnn = ProprioCNN(proprio_in_dim)
-        self.fc = nn.Linear(39 * 2 + camera_param_dim + depth_cnn_out_dim, adapt_out_dim)
+        self.proprio_cnn = ProprioCNN(proprio_hist_dim)
+        self.fc1 = nn.Linear(2 * proprio_features_dim + camera_param_dim + depth_cnn_out_dim, adapt_out_dim)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(adapt_out_dim, adapt_out_dim)
 
     def forward(self, x):
-        # proprioception, perception, camera parameters
+        # proprioception, sensor data (depth camera data), camera parameters
         proprio, sensor_data, sensor_param = x['proprio'], x['sensor_data'], x['sensor_param']
         proprio = self.proprio_cnn(proprio)
         obs = [proprio]
-        if self.use_depth:
-            sensor_data = self.depth_cnn(sensor_data)
-            obs.extend([sensor_data, sensor_param])
-        x = self.fc(torch.cat(obs, dim=-1))
+        sensor_data = self.depth_cnn(sensor_data)
+        obs.extend([sensor_data, sensor_param])
+        x = self.fc1(torch.cat(obs, dim=-1))
         x = self.fc2(self.relu(x))
         return x
 
@@ -218,8 +216,9 @@ class DepthCNN(nn.Module):
         self.fc2 = nn.Linear(256, out_dim)
     
     def forward(self, x):
-        # x has shape [num_envs, times, 1, h, w]
-        x = x.squeeze(2)
+        # FIXME: x has shape [num_envs, times, 1, h, w]
+        x = x.to(torch.float32)
+        x = rearrange(x, "b h w c -> b c h w")
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu1(x)
@@ -232,62 +231,65 @@ class DepthCNN(nn.Module):
         x = self.bn3(x)
         x = self.relu3(x)
         x = self.pool3(x)
-        x = x.view(-1, 128 * 4 * 4)
+        x = x.reshape(-1, 128 * 4 * 4)
         x = self.fc1(x)
         x = self.relu4(x)
         x = self.fc2(x)
         return x
+    
+def calc_activation_shape_1d(dim, ksize, stride=1, dilation=1, padding=0):
+
+    def shape_each_dim():
+        odim_i = dim + 2 * padding - dilation * (ksize - 1) - 1
+        return (odim_i / stride) + 1
+
+    return int(shape_each_dim())
 
 class ProprioCNN(nn.Module):
     def __init__(self, in_dim) -> None:
         super().__init__()
         # FCN for refining individual feature dimensions without considering temporal structure
-        # in_dim = 39 = agent state info dim + actions dim = agent proprio + base pose + tcp pose + actions dim
+        # in_dim = 26 = agent state info dim + actions dim = agent proprio + base pose + tcp pose + actions dim
         self.channel_transform = nn.Sequential(
-            nn.Linear(39, 39),
-            nn.LayerNorm(39),
+            nn.Linear(26, 26),
+            nn.LayerNorm(26),
             nn.ReLU(inplace=True),
-            nn.Linear(39, 39),
-            nn.LayerNorm(39),
+            nn.Linear(26, 26),
+            nn.LayerNorm(26),
             nn.ReLU(inplace=True),
         )
-        # add lLayerNorm after each Conv1d layer
-        ln_shape = calc_activation_shape_1d(in_dim, 9, 2) # ln_shape = 21
-        ln1 = nn.LayerNorm((39, ln_shape))
-        ln_shape = calc_activation_shape_1d(ln_shape, 7, 2) # ln_shape = 17
-        ln2 = nn.LayerNorm((39, ln_shape))
-        ln_shape = calc_activation_shape_1d(ln_shape, 5, 1) # ln_shape = 13
-        ln3 = nn.LayerNorm((39, ln_shape))
-        ln_shape = calc_activation_shape_1d(ln_shape, 3, 1) # ln_shape = 11
-        ln4 = nn.LayerNorm((39, ln_shape))
+        # NOTE: change CNN layer sizes here? probably excessive
         # 1D CNN for capturing temporal patterns in proprioceptive data
+        # add layerNorm after each conv1d
+        ln_shape = calc_activation_shape_1d(in_dim, 9, 2)
+        ln1 = nn.LayerNorm((26, ln_shape))
+        ln_shape = calc_activation_shape_1d(ln_shape, 7, 2)
+        ln2 = nn.LayerNorm((26, ln_shape))
+        ln_shape = calc_activation_shape_1d(ln_shape, 5, 1)
+        ln3 = nn.LayerNorm((26, ln_shape))
+        ln_shape = calc_activation_shape_1d(ln_shape, 3, 1)
+        ln4 = nn.LayerNorm((26, ln_shape))
         self.temporal_aggregation = nn.Sequential(
-            nn.Conv1d(39, 39, (9,), stride=(2,)),
+            nn.Conv1d(26, 26, (9, ), stride=(2, )),
             ln1,
             nn.ReLU(inplace=True),
-            nn.Conv1d(39, 39, (7,), stride=(2,)),
+            nn.Conv1d(26, 26, (7, ), stride=(2, )),
             ln2,
             nn.ReLU(inplace=True),
-            nn.Conv1d(39, 39, (5,), stride=(1,)),
+            nn.Conv1d(26, 26, (5, ), stride=(1, )),
             ln3,
             nn.ReLU(inplace=True),
-            nn.Conv1d(39, 39, (3,), stride=(1,)),
+            nn.Conv1d(26, 26, (3, ), stride=(1, )),
             ln4,
             nn.ReLU(inplace=True),
         )
 
-    def forward(self, prop):
-        prop = self.channel_transform(prop)  # (N, 50, 39)
-        prop = prop.permute((0, 2, 1))  # (N, 39, 50)
-        prop = self.temporal_aggregation(prop)  # (N, 39, 3)
-        prop = prop.flatten(1)
-        return prop
-
-def calc_activation_shape_1d(dim, ksize, stride=1, dilation=1, padding=0):
-    def shape_each_dim():
-        odim_i = dim + 2 * padding - dilation * (ksize - 1) - 1
-        return (odim_i / stride) + 1
-    return int(shape_each_dim())
+    def forward(self, proprio):
+        proprio = self.channel_transform(proprio)       # [N, 50, 26] = [batch_size, sequence_length, in_channels or features per timestep]
+        proprio = rearrange(proprio, "b c l -> b l c")  # [N, 26, 50]
+        proprio = self.temporal_aggregation(proprio)    # [N, 26, 2]
+        proprio = proprio.flatten(1)                    # [N, 52]
+        return proprio
 
 # Logging of scalar values during training or evaluation (to WandB or TensorBoard)
 class Logger:
